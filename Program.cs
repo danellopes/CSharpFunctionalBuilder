d389da5739c58345b74c41caa46e35ab334bcd23
danellopes <daniel.nunes.meira@outlook.com>
2024-01-21 15:56:43 -0300

    FunctionalBuilder

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace FunctionalBuilder;
+
+public class Person
+{
+    public string Name, Position, Hobby;
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}, {nameof(Hobby)}: {Hobby}";
+    }
+}
+
+public abstract class FunctionalBuilder<TSubject, TSelf>
+where TSelf : FunctionalBuilder<TSubject, TSelf>
+where TSubject : new()
+{
+    private readonly List<Func<Person, Person>> Actions = new List<Func<Person, Person>>();
+
+    public TSelf Do(Action<Person> action) => AddAction(action);
+
+    public Person Build() => Actions.Aggregate(new Person(), (p, f) => f(p));
+
+    private TSelf AddAction(Action<Person> action)
+    {
+        Actions.Add(p =>
+        {
+            action(p);
+            return p;
+        });
+
+        return (TSelf)this;
+    }
+}
+
+public sealed class PersonBuilder : FunctionalBuilder<Person, PersonBuilder>
+{
+    public PersonBuilder Called(string name) => Do(p => p.Name = name);
+}
+
+public static class PersonBuilderExtensions
+{
+    public static PersonBuilder WorksAs(this PersonBuilder builder, string position) => builder.Do(p => p.Position = position);
+    public static PersonBuilder HasFunWith(this PersonBuilder builder, string hobby) => builder.Do(p => p.Hobby = hobby);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new PersonBuilder()
+        .Called("Sarah")
+        .WorksAs("Developer")
+        .HasFunWith("Video games")
+        .Build();
+
+        System.Console.WriteLine(person);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+### Example of the Builder Design Pattern (Functional)
+
+This example of the functional builder design pattern was develop using C#.
+
+This technique is a way of expanding the function of a sealed builder (which cannot be used as extension), by structuring the builder itself in a way that it can take functions as parameters, this way, you can use a static class with static methods to pass this functions, filling out new fields.
+
+If you're interested in the udemy course by [Dmitri Nesteruk](https://www.udemy.com/user/dmitrinesteruk/) [link](https://www.udemy.com/course/design-patterns-csharp-dotnet).
